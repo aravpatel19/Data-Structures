304ea094dc6899f23d2c840a4d9d73deaebdf036
aravpatel19 <45393174+aravpatel19@users.noreply.github.com>
2020-12-07 15:08:25 -0500

    Added HashSets to ArrayList

---
+++ HashSetTreeSetAssignment/src/HashSetTreeSetAssignment.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+
+public class HashSetTreeSetAssignment {
+
+    public HashSetTreeSetAssignment(){
+
+        ArrayList<HashSet<Integer>> list = new ArrayList<>();
+        int r = (int)(Math.random()*11)+2;
+
+        for(int i=0; i<r; i++) {
+            HashSet<Integer> hashSet = new HashSet<>();
+            for(int j=0; j<10; j++){
+                int rand = (int)(Math.random()*30)+1;
+                hashSet.add(rand);
+            }
+            list.add(hashSet);
+        }
+
+        //System.out.println(list);
+
+
+    }
+
+    public HashSet<Integer> intersection(HashSet<Integer> set1, HashSet<Integer> set2){
+
+
+        return new HashSet<>();
+    }
+
+    public static void main(String[]args){
+        HashSetTreeSetAssignment app = new HashSetTreeSetAssignment();
+    }
+}
